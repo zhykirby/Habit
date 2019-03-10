8ad275b9767fae795bc0e383d7fc50f9767f77fa
zhykirby <zhykirby@qq.com>
2019-03-10 23:41:54 +0800

    admin.js
    add admin.js and content.html

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+$(function(){
+    //暂时从注册按钮直接转到用户界面
+    $('#register').click(function(){
+
+    })
+})
--- routers/admin.js
+++ routers/admin.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var routers = express.Router();
 
+routers.get('/',function(req,res,next){
+    res.render('admin/content');
+});
 
 module.exports = routers;
